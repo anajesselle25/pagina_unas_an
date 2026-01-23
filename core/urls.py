40be0a77737020cd9b4bd0208388bcb14bc21d63
anajesselle25 <anajesselle05@gmail.com>
2026-01-23 17:46:14 -0400

    completacion del html y css

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index1/', views.index1, name='index1'),
+    path('login/', views.login, name='login'),
+    path('maquillaje/', views.maquillaje , name='maquillaje'),
+    path('registro/', views.registro, name='registro'),
+    path('reservacion/', views.reservacion , name='reservacion'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def index1(request):
+    return render(request, 'core/index1.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def maquillaje(request):
+    return render(request, 'core/maquillaje.html')
+
+def registro(request):
+    return render(request, 'core/registro.html')
+
+def reservacion(request):
+    return render(request, 'core/reservacion.html')
