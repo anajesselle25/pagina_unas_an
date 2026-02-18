6388874c541e13f95577c5ebd175c684e4a8a4dc
capitanleo360 <capitanleo360@gmail.com>
2026-02-18 16:26:26 -0400

    ana no me pegues

---
+++ core/management/commands/seeders.py
@@ -0,0 +1,23 @@
+from django .core.management.base import BaseCommand
+from core.models import ServiceType
+
+class Command(BaseCommand):
+    help = 'Sembrar datos iniciales en la base de datos'
+
+    def handle(self, *args, **options):
+        # Seed ServiceType
+        self.seed_service_types()
+        self.stdout.write(self.style.SUCCESS('Datos iniciales sembrados exitosamente.'))
+
+
+    def seed_service_types(self):
+        service_types = [
+            {'name': 'Manicure'},
+            {'name': 'Pedicure'},
+        ]
+        for service_type in service_types:
+            service_type, created = ServiceType.objects.get_or_create(**service_type)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Tipo de Servicio "{service_type.name}" creado.'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Tipo de Servicio "{service_type.name}" ya existe.'))
